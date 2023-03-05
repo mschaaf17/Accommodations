0fabf9fe304c7d604cecf6012aa0c513b7eabbd0
Maddy SCHAAF <maddy.schaaf@biofiredx.com>
2023-03-05 11:17:33 -0700

    added info for student charts

--- client/src/pages/StudentCharts/index.js
+++ client/src/pages/StudentCharts/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom'
 
+// Student Charts for frequency, duration?? eloping/aggression/other?, observation form, abc data   
 export default function StudentCharts() {
   return (
     <div>
-      Student Charts for frequency, duration?? eloping/aggression/other?, observation form, abc data   
+      <div>
+        <button><Link to ={`/`}>Provide more Information on this student</Link></button>
+        {/* this button should be similar to a bip or the questionairre for student behavior such as a FBA */}
+      <h2>Data</h2>
+      <div>Frequency</div>
+      <div>Duration</div>
+      <div>Contract</div>
+      <div>ABC???</div>
+        </div>
+      <div>
+        Intervention Suggestions:
+      </div>
       <div> <Link className="link-to-page logout" to ={`/teacherdata`}> ← Back to Student List</Link></div>
     </div>
   )
