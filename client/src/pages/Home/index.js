2b8c55540feeab6216d00e4c4638bb1238eb90fe
Maddy SCHAAF <maddy.schaaf@biofiredx.com>
2023-06-18 12:52:42 -0600

    took of header

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <div className='container'>
-      <h2>Start Getting Support</h2>
+      {/* <h2>Start Getting Support</h2> */}
         {/* <h1><span className ="italics">Always</span><br />get the <br /><span className ="bold">Support</span> <br /><span className="you">YOU</span><br /> need</h1> */}
        <div className='start-section'>
        <Link className ="btn-home start-btn" to="/login">Start</Link>
