13ae81d7856a0810485646771ff7b9bc7a41a310
Maddy SCHAAF <maddy.schaaf@biofiredx.com>
2023-06-18 11:21:53 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // mongoose port name
 mongoose.connect(
-    process.env.MONGDB_URI || 'mongodb://localhost:27017/inclusion-student-app',
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/inclusion-student-app',
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
