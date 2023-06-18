5e798aba3a56976b8928635b970032d5d39448d9
Maddy SCHAAF <maddy.schaaf@biofiredx.com>
2023-06-18 11:54:15 -0600

    seeded data

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -2,17 +2,17 @@
 const userSeeds = require('./userSeed.json');
 const accommSeeds = require('./accommSeed.json');
 const db = require('../config/connection');
-const {Accommodation, User } = require('../models');
+const {AccommodationCards, User } = require('../models/index');
 
 db.once('open', async () => {
   try {
-    await Accommodation.deleteMany({});
+    await AccommodationCards.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
     for (let i = 0; i < accommSeeds.length; i++) {
-      const { _id, accommAuthor } = await Accommodation.create(accommSeeds[i]);
+      const { _id, accommAuthor } = await AccommodationCards.create(accommSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: accommAuthor },
         {
